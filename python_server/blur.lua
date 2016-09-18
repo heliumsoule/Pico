7d72d97f8571c80958be274bb452599c9b62c14b
Patrick Tsai <penguin@patosai.com>
2016-09-18 05:13:40 -0400

    Fix lua script for gaussian blur

--- python_server/blur.lua
+++ python_server/blur.lua
@@ -2,6 +2,8 @@ require 'image'
 
 img = image.load('/Users/patosai/Downloads/combined.jpeg')
 img_mid = img:narrow(3, 462, 100)
-k = image.gaussian(3)
-img_mid = image.convolve(img_mid, k, 'same')
+print(img:sum())
+k = image.gaussian(2)
+img_mid:copy(image.convolve(img_mid, k, 'same'))
+print(img:sum())
 image.save('/Users/patosai/Downloads/combined_blur.jpeg', img)
